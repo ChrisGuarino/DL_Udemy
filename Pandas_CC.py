af43fae19cc7933b0065e5db52d7f412a60a1346
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2022-07-28 15:19:59 -0400

    Panads_CC

--- Pandas_CC.py
+++ Pandas_CC.py
@@ -29,6 +29,7 @@ print(ser1['USA'])
 ser2 = pd.Series([5,6,7,8],['USA','Germany','Italy','Japan'])
 print(ser1 +ser2)  
 """ 
+
 #Dataframe - like a data table 
 #A Dataframe is more than one Series that share one index
 
@@ -50,9 +51,43 @@ print(df['W'])
 mylist = ['W','Y']
 print(df[mylist])
 
-#Creat a new Column 
+#Create a new Column 
 df['New'] = df['W'] + df['Y'] 
 print(df) 
 
-#To remove a column, this also works for rows. Keep in mind you won't need the axis=1. 
+#To remove a column, this also works for rows. Just remove the "axis" arguement. Sets default to 0. Keep in mind you won't need the axis=1. This is not presistent. 
 df.drop('New',axis=1)
+#If you want it to be presistent you add "inplace=True" argument 
+
+#Access a single Row 
+print("Row:\n",df.loc['A']) 
+#You can also use the numpy index location instead of the Pandas label 
+print("Row:\n",df.iloc[0])
+
+#Select mutliple rows 
+print("Multiple Row\n", df.loc[['A','E']]) #For mutliples you just need to pass in a list of rows. 
+
+#Select Multiple Columns 
+print("Mutliple Columns:\n",df.loc[['A','B'],['Y','Z']])#Use multiple lists. Rows then columns
+
+
+#Conditional Selection 
+print(df>0) 
+df_bool = df >0 
+
+#We can now pass this into our dataframe and it will give us only the fields that return True. 
+print("Conditional Selection:\n", df[df_bool]) #Passing a filtered row into the dataframe. Only return the values that mett the condition.
+print("Conditional Filtering:\n", df[df['W']>0]) #More common way of seeing this
+
+#Filtering the dataframe for conditions 
+print("Conditional Filtering:\n",df['W']>0)  
+
+#You can stack the operations. This gives the ability to select specific data from a dataframe. 
+print("Operations Stacking:\n",df[df['W']>0]['Y'].loc['A']) 
+
+#Two Coniditions or More 
+cond_1 = df['W']>0
+cond_2 = df['Y']>1
+
+
+
